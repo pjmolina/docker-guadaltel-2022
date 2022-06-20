8f3fbd3f03d30e71df95a04cd42ba5f83edf6e4d
Pedro J. Molina <pjmolina@gmail.com>
2022-06-20 19:07:47 +0200

    3 instances

--- node-sample/docker-run.sh
+++ node-sample/docker-run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-docker run --name web1 -p 5001:9099  nodeweb
+docker run -d --name web1 -p 5001:9099  nodeweb
+docker run -d --name web2 -p 5002:9099  nodeweb
+docker run -d --name web3 -p 5003:9099  nodeweb
