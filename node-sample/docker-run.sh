8ddda7ebf4a00985751a50d3d03fad5fd75dc98a
Pedro J. Molina <pjmolina@gmail.com>
2022-06-22 19:21:06 +0200

    session 3. compose + nginx load-balancing sample

--- node-sample/docker-run.sh
+++ node-sample/docker-run.sh
@@ -2,8 +2,17 @@
 
 docker rm web1 web2 web3 -f
 
-docker run -d --name web1 -e APP_NAME=''    -p 5001:80  nodeweb
-docker run -d --name web2 -e APP_NAME=mercurio -p 5002:80  nodeweb
+docker volume create volumen1
+
+docker run -d --name web1 -e APP_NAME=''  -p 5001:80 \
+    -v "volumen1:/app-log" \
+    nodeweb   
+
+docker run -d --name web2 -e APP_NAME=mercurio -p 5002:80 \
+  -v "volumen1:/app-log" \
+  nodeweb
+
+
 docker run -d \
   --name web3 \
   -e APP_NAME=jupiter \
